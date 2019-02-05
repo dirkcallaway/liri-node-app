098d5f5c9550402e2295878406496bbcd0c7ebc0
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-02-04 20:54:27 -0700

    Concert search is functional

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@
 // var spotify = new Spotify(keys.spotify);
 
 var inquirer = require("inquirer");
+var axios = require("axios");
 
 inquirer.prompt([
 
@@ -19,7 +20,35 @@ inquirer.prompt([
   // Split case based on response above
   switch (liri.command) {
     case "Find concerts.":
-      console.log("Concerts...");
+      inquirer.prompt([
+
+        {
+          type: "input",
+          name: "band",
+          message: "What band are you searching for?"
+        }
+
+      ]).then(function (band) {
+        var artistFullName = band.band;
+        var bandName = artistFullName.split(" ");
+        var artist = bandName.join("%20");
+        var bandQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+        axios.get(bandQuery).then(
+          function (response) {
+            if(response.data.length > 0){
+            console.log("\n" + artistFullName + " are playing the following shows:");
+            for (var i = 0; i < response.data.length; i++) {
+              console.log("---------------------------------");
+              console.log("Venue Name: " + response.data[i].venue.name);
+              console.log("Venue Location: " + response.data[0].venue.city + ", " + (response.data[0].venue.region));
+              console.log("Event Date: " + response.data[0].datetime);
+            }
+          }else {
+            console.log("\n" + artistFullName + " have no shows scheduled.");
+          }
+          }
+        );
+      });
       break;
     case "What song is this?":
       text = "Song...";
